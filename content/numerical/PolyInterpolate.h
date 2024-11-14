fdf956cdc9449d2c6854aad9c02ad25f6bca95f7
lmqzzz <86908618+lmqzzz@users.noreply.github.com>
2024-11-15 01:16:46 +0700

    Update PolyInterpolate.h

--- content/numerical/PolyInterpolate.h
+++ content/numerical/PolyInterpolate.h
@@ -6,20 +6,78 @@
  * Description: Given $n$ points (x[i], y[i]), computes an n-1-degree polynomial $p$ that
  *  passes through them: $p(x) = a[0]*x^0 + ... + a[n-1]*x^{n-1}$.
  *  For numerical precision, pick $x[k] = c*\cos(k/(n-1)*\pi), k=0 \dots n-1$.
- * Time: O(n^2)
+ * Time: O(n^2) -> O(nlog(mod))
+ */
+
+/*
+ * Complexity: O(Nlog(mod), N)
  */
 #pragma once
 
-typedef vector<double> vd;
-vd interpolate(vd x, vd y, int n) {
-	vd res(n), temp(n);
-	rep(k,0,n-1) rep(i,k+1,n)
-		y[i] = (y[i] - y[k]) / (x[i] - x[k]);
-	double last = 0; temp[0] = 1;
-	rep(k,0,n) rep(i,0,n) {
-		res[i] += y[k] * temp[i];
-		swap(last, temp[i]);
-		temp[i] -= last * x[k];
-	}
-	return res;
+#define IP Interpolation
+namespace Interpolation {
+const int mod = (int)1e9 + 7;
+const int maxn = 1e5 + 5;
+int a[maxn];
+int fac[maxn];
+int ifac[maxn];
+int prf[maxn];
+int suf[maxn];
+
+int fpow(int n, int k) {
+  int r = 1;
+  for (; k; k >>= 1) {
+    if (k & 1)
+      r = (long long)r * n % mod;
+    n = (long long)n * n % mod;
+  }
+  return r;
 }
+void upd(int u, int v) { a[u] = v; }
+void build() {
+  fac[0] = ifac[0] = 1;
+  for (int i = 1; i < maxn; i++) {
+    fac[i] = (long long)fac[i - 1] * i % mod;
+    ifac[i] = fpow(fac[i], mod - 2);
+  }
+}
+// Calculate P(x) of degree k - 1, k values form 1 to k
+// P(i) = a[i]
+int calc(int x, int k) {
+  prf[0] = suf[k + 1] = 1;
+  for (int i = 1; i <= k; i++) {
+    prf[i] = (long long)prf[i - 1] * (x - i + mod) % mod;
+  }
+  for (int i = k; i >= 1; i--) {
+    suf[i] = (long long)suf[i + 1] * (x - i + mod) % mod;
+  }
+  int res = 0;
+  for (int i = 1; i <= k; i++) {
+    if (!((k - i) & 1)) {
+      res = (res + (long long)prf[i - 1] * suf[i + 1] % mod * ifac[i - 1] %
+                       mod * ifac[k - i] % mod * a[i]) %
+            mod;
+    } else {
+      res = (res -
+             (long long)prf[i - 1] * suf[i + 1] % mod * ifac[i - 1] % mod *
+                 ifac[k - i] % mod * a[i] % mod +
+             mod) %
+            mod;
+    }
+  }
+  return res;
+}
+}  // namespace Interpolation
+
+const int mod = (int)1e9 + 7;
+
+// int main() {
+//   IP::build();
+//   for (int i = 1; i < IP::maxn; i++) {
+//     IP::a[i] =
+//         ((long long)3111 * i * i * i - (long long)54 * i * i + 13 * i) % mod;
+//   }
+//   assert(IP::calc(1234, 4) == IP::a[1234]);
+//   cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
+//   return 0;
+// }
